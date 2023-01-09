49e2385af4411e2ae19ec3983a27830aabd7c956
squirrelnika <nika.valerija@gmail.com>
2023-01-09 17:07:35 +0200

    icon

--- partials/header.php
+++ partials/header.php
@@ -6,6 +6,7 @@
     <title> <?php echo $title ?> </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="css/styles.css">
+    <link rel="icon" href="css\favicon.ico">
 </head>
 <body>
     <div class="container">
